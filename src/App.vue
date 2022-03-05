2b910dd598693315c24314a37001bce6326dc921
Lee, Jeffrey <jl187@illinois.edu>
2022-03-05 14:10:14 -0800

    Update Raindew 3
    UBRG Test - Combat 1

--- src/App.vue
+++ src/App.vue
@@ -102,11 +102,11 @@ export default {
       "pilots": [
         {
           "callsign": "Raindew",
-          "alias": "Aerias",
+          "alias": "Aeris",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Oworest"
+          "mech": "Angel"
         },
         {
           "callsign": "Nightwitch",
