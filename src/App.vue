c3e01abaf507b38ce38afe20848eb4196caccbe3
Lee, Jeffrey <jl187@illinois.edu>
2022-04-17 17:30:19 -0700

    player mech stuff

--- src/App.vue
+++ src/App.vue
@@ -109,41 +109,41 @@ export default {
           "callsign": "Raindew",
           "alias": "Aeris",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Angel"
+          "corpro": "Horus",
+          "frame": "Pegasus",
+          "mech": "Angel MKII"
         },
         {
           "callsign": "Nightwitch",
           "alias": "Karl Otto Von Havich",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
+          "corpro": "SSC",
+          "frame": "Black Witch",
           "mech": "Karl's Marvelous Wish Granting Engine"
         },
         {
           "callsign": "Maze 3",
           "alias": "Lance Armstrong",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
-          "corpro": "GMS",
-          "frame": "Everest",
+          "corpro": "Horus",
+          "frame": "Minotaur",
           "mech": "Novelty Theory"
         },
         {
           "callsign": "Eveready",
           "alias": "Quincy Jones",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Stonewall"
+          "corpro": "IPS-N",
+          "frame": "Tortuga",
+          "mech": "True Stonewall"
         },
         {
           "callsign": "Passenger Pigeon",
           "alias": "Steve Nelson",
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Skylord"
+          "corpro": "SSC",
+          "frame": "Monarch",
+          "mech": "Skylord Mk.2"
         },
       ],
       "header": {
